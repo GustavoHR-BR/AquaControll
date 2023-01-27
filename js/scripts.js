429d0b59ced7da5edcbb13e1277606f4cd1438f0
Gustavo <gustavohique12@gmail.com>
2023-01-26 21:11:48 -0300

    Login form

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
 Parse.initialize("AppKey", "KeyJS");
 Parse.serverURL = "https://parseapi.back4app.com/";
+
+function logIn() {
+    const userValue = document.getElementById("user").value;
+    const passValue = document.getElementById("pass").value;
+    
+    if (userValue.length <= 0 || passValue.length <= 0) {
+        alert("Usuário ou senha inválidos!");
+    } else {
+        var user = Parse.User.logIn(userValue, passValue).then(function(user) {
+            console.log(user.IdComunidade);
+            window.location.href = "main.html";    
+        }).catch(function(error){
+            alert("Usuário ou senha incorretos!");
+            console.log("Error: " + error.code + " " + error.message);
+        });
+    }
+}
