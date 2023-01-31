ee55005baa36284b81a44322e8a83eabe85a180a
Gustavo <gustavohique12@gmail.com>
2023-01-30 22:57:17 -0300

    Ajustado para utilizar os dados de usuário do próprio Parse no localStorage; Implementado layout base utilizando Bootstrap.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,47 @@
 Parse.initialize("AppKey", "KeyJS");
 Parse.serverURL = "https://parseapi.back4app.com/";
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-async function onLoadIndex(){
-    if (localStorage.getItem('UserId') != null) {
+function onLoadIndex(){
+    if (localStorage.getItem('Parse/9DLVirQkTY8Gy5AnLJEyCTPFi41W5a3FkSoCVtfa/currentUser') != null && localStorage.getItem("Finalizou") == "false") {
         window.location.href = "main.html";    
     }
 }
 
-async function onLoadMain(){
-    if (localStorage.getItem('UserId') == null) {
+function onLoadMain(){
+    if (localStorage.getItem('Parse/9DLVirQkTY8Gy5AnLJEyCTPFi41W5a3FkSoCVtfa/currentUser') == null) {
         window.location.href = "index.html";    
     }else {
         try {
-            const Comunidade = Parse.Object.extend("comunidade");
-            const comunidade = new Parse.Query(Comunidade);
-            comunidade.get(localStorage.getItem("ComunidadeId"))
-            .then((c) => {
-               console.log("Sucess");
-            }, (error) => {
-               console.log("Error: " + error.code + " " + error.message);    
+            localStorage.setItem("Finalizou", "false");
+            var idComunidade = JSON.parse(localStorage.getItem('Parse/9DLVirQkTY8Gy5AnLJEyCTPFi41W5a3FkSoCVtfa/currentUser')).IdComunidade;
+            comunidade = new Parse.Query(Parse.Object.extend("comunidade"));
+            comunidade.equalTo("objectId", idComunidade);
+            comunidade.first().then(function(c){
+                if(c){
+                    // Encontrou, basta usar c
+                } else {
+                   console.log("Comunidade não encontrada!");
+                }
             });
-
-            document.getElementById("comunidade").value = comunidade.get("nome");
         } catch (error) {
             console.log("Error: " + error.code + " " + error.message);    
         }
     }
 }
 
+function freeOnClose(){
+    if (localStorage.getItem("SalvouDados") == "false"){
+        localStorage.clear();
+    }
+    localStorage.setItem("Finalizou", "true");
+    window.location.href = "index.html";
+}
+
 async function logIn() {
     const userValue = document.getElementById("user").value;
     const passValue = document.getElementById("pass").value;
+    localStorage.setItem("SalvouDados", document.getElementById("check-remember").checked);
     //Fazer mais validações no login
     
     if (userValue.length <= 0 || passValue.length <= 0) {
@@ -38,14 +49,13 @@ async function logIn() {
     } else {
         try{
             if (Parse.User.logIn(userValue, passValue)) {
-                const currentUser = Parse.User.current();
-                localStorage.setItem('UserId', currentUser.id);
-                localStorage.setItem('ComunidadeId', currentUser.get("IdComunidade"));
+                await delay(700); // Aguardar pra garantir que salvou o User no localStorage
                 window.location.href = "main.html";    
             }else{
                 alert("Usuário ou senha incorretos!");   
+                document.getElementById("pass").value = "";
             }
-        }catch{
+        }catch (error) {
             console.log("Error: " + error.code + " " + error.message);    
         }
     }
